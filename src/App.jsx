90ae932adbd8571e2ba61667e55d74679b2e018c
MehediHasan06 <mehedihasanmasud06@gmail.com>
2023-03-31 23:30:00 +0600

    interactivity added

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,42 @@ import { useState } from 'react';
 import './App.css';
 
 function Board() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <div className="board-row">
-        <Squre value={1}/>
-        <Squre value={2}/>
-        <Squre value={3}/>
+        <Squre/>
+        <Squre/>
+        <Squre/>
       </div>
       <div className="board-row">
-        <Squre value={4}/>
-        <Squre value={5}/>
-        <Squre value={6}/>
+        <Squre/>
+        <Squre/>
+        <Squre/>
       </div>
       <div className="board-row">
-        <Squre value={7}/>
-        <Squre value={8}/>
-        <Squre value={9}/>
+        <Squre/>
+        <Squre/>
+        <Squre/>
       </div>
     </div>
   )
 };
-function Squre({value}) {
-  return <button className="squre">{value}</button>
+function Squre() {
+
+  const [value, setValue] = useState(null);
+  const handleClick = () => {
+    setValue('X');
+  };
+
+  return (
+    <button 
+      className="squre"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default Board;

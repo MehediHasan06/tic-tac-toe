0f587326c929ea5a9f1b2d1642384f4a55326955
MehediHasan06 <mehedihasanmasud06@gmail.com>
2023-04-01 01:42:02 +0600

    state lift to the parent comps

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,40 @@ import { useState } from 'react';
 import './App.css';
 
 function Board() {
+  const [squres, setSqures] = useState(Array(9).fill(null));
+  const handleSqureClick = (i) => {
+    let nextSqures = squres.slice();
+    console.log(nextSqures);
+    nextSqures[i] = "X";
+    setSqures(nextSqures);
+  };
+
 
   return (
     <div className="App">
       <div className="board-row">
-        <Squre/>
-        <Squre/>
-        <Squre/>
+        <Squre value={squres[0]} handleSqureClick={() => handleSqureClick(0)}/>
+        <Squre value={squres[1]} handleSqureClick={() => handleSqureClick(1)}/>
+        <Squre value={squres[2]} handleSqureClick={() => handleSqureClick(2)}/>
       </div>
       <div className="board-row">
-        <Squre/>
-        <Squre/>
-        <Squre/>
+        <Squre value={squres[3]} handleSqureClick={() => handleSqureClick(3)}/>
+        <Squre value={squres[4]} handleSqureClick={() => handleSqureClick(4)}/>
+        <Squre value={squres[5]} handleSqureClick={() => handleSqureClick(5)}/>
       </div>
       <div className="board-row">
-        <Squre/>
-        <Squre/>
-        <Squre/>
+        <Squre value={squres[6]} handleSqureClick={() => handleSqureClick(6)}/>
+        <Squre value={squres[7]} handleSqureClick={() => handleSqureClick(7)}/>
+        <Squre value={squres[8]} handleSqureClick={() => handleSqureClick(8)}/>
       </div>
     </div>
   )
 };
-function Squre() {
-
-  const [value, setValue] = useState(null);
-  const handleClick = () => {
-    setValue('X');
-  };
-
+function Squre({ value, handleSqureClick }) {
   return (
     <button 
       className="squre"
-      onClick={handleClick}
+      onClick={handleSqureClick}
     >
       {value}
     </button>
